1ee547cb7ad0f2edcb8529af5d4347fca515d588
marks12 <webts@bk.ru>
2014-06-27 23:46:09 +0700

    Module.php correct

--- Module.php
+++ Module.php
@@ -4,5 +4,5 @@
 * It allows usage of this module even without composer.
 * The original Module.php is in 'src/TsvFunctions' in order to respect PSR-0
 */
-
+ 
 require_once __DIR__ . '/src/TsvFunctions/Module.php';
