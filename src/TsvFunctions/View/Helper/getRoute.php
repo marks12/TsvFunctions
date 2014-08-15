87efd2e394cf3bbb38012ba1c3c94247c7c261ec
marks12 <webts@bk.ru>
2014-08-16 06:50:31 +0700

    modify

--- src/TsvFunctions/View/Helper/getRoute.php
+++ src/TsvFunctions/View/Helper/getRoute.php
@@ -1,22 +1,19 @@
 <?php
-namespace TsvFunctions\View\Helper;
 use Zend\View\Helper\AbstractHelper;
 
 class getRoute extends AbstractHelper {
 	private $sm;
 
-    public function __construct($app) {
-        $this->sm = $app->getServiceManager();
-    }
+	public function __construct($app) {
+		$this->sm = $app->getServiceManager();
+	}
 
-	public function __invoke()
-	{
+	public function echoRoute() {
 		$router = $this->sm->get('router');
 		$request = $this->sm->get('request');
 
 		$routeMatch = $router->match($request);
-		
 		if (!is_null($routeMatch))
-			return $routeMatch->getMatchedRouteName();
+			echo $routeMatch->getMatchedRouteName();
 	}
 }
